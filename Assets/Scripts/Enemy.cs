87bd99b215d0818ee5d7ee881f62c3043bca76c1
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-03-08 12:31:34 +0100

    Update Enemy.cs: Added placeholder skeleton logic to be fleshed out

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    /// <summary>
+    /// this script goes on each Enemy and monitors death logic
+    /// </summary>
+
+
+    private void OnDeath()
+    {
+        // var room = ...
+        // access Room Script via parent
+
+        // update EnemyCount = EnemyCount -1:
+    }
 
 }
