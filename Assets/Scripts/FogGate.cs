9fa8c02da129254e5f28b64d7b280ebc4367117d
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-03-08 12:31:44 +0100

    Update FogGate.cs: Added placeholder skeleton logic to be fleshed out

--- Assets/Scripts/FogGate.cs
+++ Assets/Scripts/FogGate.cs
@@ -3,4 +3,23 @@ using UnityEngine;
 public class FogGate : MonoBehaviour
 {
 
+    /// <summary>
+    /// Toggles invisible walls on and off, probably placed on Hallways that connect rooms and block/ allow passage
+    /// </summary>
+
+
+    // each hallway will have two colliders and two fog particle effects
+
+    // fed a box collider and a particle effect to know which ones to toggle
+    private void DisableInvisibleWall(BoxCollider boxCollider, ParticleSystemRenderer fogParticleEffect)
+    {
+        boxCollider.enabled = false;
+        fogParticleEffect.enabled = false;
+    }
+
+    private void EnableInvisibleWall(BoxCollider boxCollider, ParticleSystemRenderer fogParticleEffect)
+    {
+        boxCollider.enabled = true;
+        fogParticleEffect.enabled = true;
+    }
 }
