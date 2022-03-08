a037e156e9bcc249cb551c292d522620abd1e0be
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-03-08 12:31:54 +0100

    Update Hallway.cs: Added placeholder skeleton logic to be fleshed out

--- Assets/Scripts/Hallway.cs
+++ Assets/Scripts/Hallway.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class Hallway : MonoBehaviour
 {
-   
+    /// <summary>
+    /// this script handles the connecting hallways, it triggers difficulty scaling up and gets rooms ready/ or makes them idle
+    /// </summary>
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Only execute if Player
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        // Activate script for next Room, unless this is automized
+        // Increase progress for difficulty scaling
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        // Only execute if Player
+        if (!other.gameObject.CompareTag("Player")) return;
+
+        // possibly deactivate last room to save space
+    }
 }
