864ef716868fdb69196ba5ba470553e0b1a46cdb
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-03-09 14:53:33 +0100

    Update DifficultyScaler.cs: cleaned up naming conventions & flooring method

--- Assets/Scripts/DifficultyScaler.cs
+++ Assets/Scripts/DifficultyScaler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class DifficultyScaler : MonoBehaviour
@@ -7,35 +8,38 @@ public class DifficultyScaler : MonoBehaviour
     /// </summary>
 
 
-    private int Enemy1Count;
-    private int Enemy2Count;
-    private int Enemy3Count;
-    private int Enemy4Count;
+    private int enemy1Count = 4;
+    private int enemy2Count;
+    private int enemy3Count;
+    private int enemy4Count;
 
-    public int RoomsCleared;
+    public int roomsClearedTotal;
 
     private void Start()
     {
-        RoomsCleared = 0;
+        roomsClearedTotal = 0;
     }
 
     private void ScaleDifficultyUp()
     {
-        Enemy1Count =+ RoomsCleared;
-        Enemy2Count =+ RoomsCleared;
+        enemy1Count += roomsClearedTotal;
+        enemy2Count += roomsClearedTotal;
 
         // do stuff like this to keep more difficult enemy amount down
         // e.g 4 roomsCleared = 2, 5 RoomsCleared = 2, 6 RoomsCleared = 3 (or 2 if floored from 2.5), 7 rooms = 3
-        int Half =+ (int) (RoomsCleared /2 - 0.5f);
-        Enemy3Count =+ Half;
+        double half = roomsClearedTotal / 2;
+        int half2 = Convert.ToInt32(Math.Floor(half));
+
+
+        enemy3Count += half2;
 
         // even smaller amount
-        Enemy4Count =+ (int) (Half / 2 - 0.5f);
+        enemy4Count += (int) Math.Floor(half / 2);
 
 
-        Debug.Log($"{Enemy1Count}");
-        Debug.Log($"{Enemy2Count}");
-        Debug.Log($"{Enemy3Count}");
-        Debug.Log($"{Enemy4Count}");
+        Debug.Log($"{enemy1Count}");
+        Debug.Log($"{enemy2Count}");
+        Debug.Log($"{enemy3Count}");
+        Debug.Log($"{enemy4Count}");
     }
 }
