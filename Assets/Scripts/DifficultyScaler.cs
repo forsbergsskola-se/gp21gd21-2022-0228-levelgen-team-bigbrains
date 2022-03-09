42c6b818bb67a2f80f382a787e339f254cde0b88
Victorpetts <vike.pet@hotmail.com>
2022-03-09 16:21:44 +0100

    Difficulity scaling work

--- Assets/Scripts/DifficultyScaler.cs
+++ Assets/Scripts/DifficultyScaler.cs
@@ -1,45 +1,36 @@
 using System;
 using UnityEngine;
 
-public class DifficultyScaler : MonoBehaviour
-{
+public class DifficultyScaler : MonoBehaviour {
     /// <summary>
     /// this script goes on an empty DifficultyManager object and increments the hordes of enemies based on roomsCleared
     /// </summary>
 
-
-    private int enemy1Count = 4;
-    private int enemy2Count;
-    private int enemy3Count;
-    private int enemy4Count;
+    public int enemy1Count = 4;
+    public int enemy2Count;
+    public int enemy3Count;
+    public int enemy4Count;
 
     public int roomsClearedTotal;
 
-    private void Start()
-    {
+    private void Start() {
         roomsClearedTotal = 0;
     }
 
-    private void ScaleDifficultyUp()
-    {
+    private void ScaleDifficultyUp() {
+        double half = roomsClearedTotal / 2;
+        int halfFloored = Convert.ToInt32(Math.Floor(half));
+        double sqrt = Math.Sqrt(roomsClearedTotal);
+        int sqrtFloored = Convert.ToInt32(Math.Floor(sqrt));
+
         enemy1Count += roomsClearedTotal;
         enemy2Count += roomsClearedTotal;
+        enemy3Count += halfFloored;
+        enemy4Count += sqrtFloored;
 
-        // do stuff like this to keep more difficult enemy amount down
-        // e.g 4 roomsCleared = 2, 5 RoomsCleared = 2, 6 RoomsCleared = 3 (or 2 if floored from 2.5), 7 rooms = 3
-        double half = roomsClearedTotal / 2;
-        int half2 = Convert.ToInt32(Math.Floor(half));
-
-
-        enemy3Count += half2;
-
-        // even smaller amount
-        enemy4Count += (int) Math.Floor(half / 2);
-
-
-        Debug.Log($"{enemy1Count}");
-        Debug.Log($"{enemy2Count}");
-        Debug.Log($"{enemy3Count}");
-        Debug.Log($"{enemy4Count}");
+        // Debug.Log($"{enemy1Count}");
+        // Debug.Log($"{enemy2Count}");
+        // Debug.Log($"{enemy3Count}");
+        // Debug.Log($"{enemy4Count}");
     }
 }
