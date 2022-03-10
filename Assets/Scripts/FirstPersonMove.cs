f97ec3acd373ed6a97ee032827a89f2d1dde3995
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-03-10 17:42:25 +0100

    Update to First Person script

--- Assets/Scripts/FirstPersonMove.cs
+++ Assets/Scripts/FirstPersonMove.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class FirstPersonMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float speed = 10.0f;
+    private float translation;
+    private float straffe;
+
+
+    void Start () {
+        // turn off the cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        // Input.GetAxis() is used to get the user's input
+        // You can furthor set it on Unity. (Edit, Project Settings, Input)
+        translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape")) {
+            // turn on the cursor
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
