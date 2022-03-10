71c4549b87e3102bafbc18a44338a7f487ebd965
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-03-10 18:12:17 +0100

    Update FogGate.cs

--- Assets/Scripts/FogGate.cs
+++ Assets/Scripts/FogGate.cs
@@ -5,22 +5,24 @@ public class FogGate : MonoBehaviour {
     /// Toggles invisible walls on and off, probably placed on Hallways that connect rooms and block/ allow passage
     /// </summary>
 
-    public BoxCollider boxCollider;
+    public BoxCollider invisibleWall;
     public ParticleSystemRenderer fogParticleEffect;
 
-    public void DisableFogGate() {
-        boxCollider.enabled = false;
+    public void DisableFogGate()
+    {
+        invisibleWall.enabled = false;
         fogParticleEffect.enabled = false;
     }
 
-    private void OnCollisionExit(Collision other) {
+    private void OnCollisionExit(Collision other)
+    {
         if (!other.gameObject.CompareTag("Player")) return;
-
         EnableFogGate();
     }
 
-    private void EnableFogGate() {
-        boxCollider.enabled = true;
+    private void EnableFogGate()
+    {
+        invisibleWall.enabled = true;
         fogParticleEffect.enabled = true;
     }
 }
