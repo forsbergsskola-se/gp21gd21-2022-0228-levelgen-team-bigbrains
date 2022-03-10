39e2e314c095b99bb9024011d16d42173d5572b6
Victorpetts <vike.pet@hotmail.com>
2022-03-10 12:37:26 +0100

    Adds chase method to enemies and comments in some health and attack code that might be used later

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,55 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
     /// <summary>
-    /// this script goes on each Enemy and monitors death logic
+    /// this script goes on each Enemy and monitors enemy logic
     /// </summary>
 
     private Spawner spawner;
+    private Transform target;
+    public int aggroRange;
+    public int moveSpeed;
+
+    // [SerializeField] private float maxHealth;
+    // [SerializeField] private float currentHealth;
+
+    // public float attackRate = 2.5f;
+    // public float attackRange = 1.5f;
+    // private float nextAttackTime;
+
+    // private float CurrentHealth {
+    //     get => currentHealth;
+    //     set {
+    //         currentHealth = value;
+    //         if (currentHealth <= 0) {
+    //             OnDeath();
+    //         }
+    //     }
+    // }
+
+    private void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        // CurrentHealth = maxHealth;
+    }
+
+    private void FixedUpdate() {
+        Chase();
+        // Attack();
+    }
+
+    // private void Attack() {
+    //     if (Vector3.Distance(transform.position, target.position) < attackRange) {
+    //         if (Time.time >= nextAttackTime) {
+    //             player.TakeDamage(1);
+    //             nextAttackTime = Time.time + 1f / attackRate;
+    //         }
+    //     }
+    // }
+
+    private void Chase() {
+        if (Vector3.Distance(transform.position, target.position) < aggroRange) {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
 
     private void OnDeath() {
         spawner = FindObjectOfType<Spawner>().GetComponent<Spawner>();
